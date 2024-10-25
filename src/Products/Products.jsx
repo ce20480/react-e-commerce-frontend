587ae9ebf257f813ba78d8dc04561834449a9e93
ce20480 <ce20480@bristol.ac.uk>
2024-10-25 17:13:21 +0100

    Product skeleton made, refractoring needed

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,6 +1,39 @@
 import "./Products.css";
+import { AiFillStar } from "react-icons/ai";
+import { BsFillBagHeartFill } from "react-icons/bs";
+
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <section className="card-container">
+        <section className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+            alt="Shoe"
+            className="card-img"
+          />
+          <div className="card-details">
+            <h3 className="card-title">Shoe</h3>
+            <section className="card-reviews">
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <AiFillStar className="rating-star" />
+              <span className="total-reviews">4</span>
+            </section>
+            <section className="card-price">
+              <div className="price">
+                Was <del>$300</del> Now $200
+              </div>
+              <div className="bag">
+                <BsFillBagHeartFill className="bag-icon" />
+              </div>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
 };
 
 export default Products;
