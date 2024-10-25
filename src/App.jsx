d39f9cc23e75015df679f12bb994866051298a7f
ce20480 <ce20480@bristol.ac.uk>
2024-10-25 16:51:02 +0100

    Navigation stylingbegan

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
+import "./index.css";
 import Nav from "./Navigation/Nav";
 import Recommended from "./Recommended/Recommended";
 import Products from "./Products/Products";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Count: {count}</h1>
       <Nav />
       <Products />
       <Recommended />
--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -1,7 +1,31 @@
+import { FiHeart, FiShoppingCart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+
 import "./Nav.css";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Enter your search shoes"
+        />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icon" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icon" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icon" />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
