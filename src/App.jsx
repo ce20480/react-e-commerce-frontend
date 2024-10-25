c67aad00a31908d6495eff889c5273a58ac6626e
ce20480 <ce20480@bristol.ac.uk>
2024-10-25 16:28:19 +0100

    Components and main building blocks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Nav from "./Navigation/Nav";
+import Recommended from "./Recommended/Recommended";
+import Products from "./Products/Products";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -6,6 +9,9 @@ function App() {
   return (
     <>
       <h1>Count: {count}</h1>
+      <Nav />
+      <Products />
+      <Recommended />
     </>
   );
 }
---
+++ src/Sidebar/Price/Price.jsx
@@ -0,0 +1,6 @@
+import "./Price.css";
+const Price = () => {
+  return <div>Price</div>;
+};
+
+export default Price;
